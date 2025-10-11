745affad913c64ed7929e9818336d545cbe8d61d
Patryk Rejent <prejent@edu.cdv.pl>
2025-10-11 14:25:01 +0200

    to resolve

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,31 @@
+﻿// 
+
+class Person
+{
+    private string name;
+    private int age;
+    
+    public Person(string name, int age)
+        {
+        this.name = name;
+        this.age = age;
+        }
+
+    public void introduceYourself()
+    {
+        Console.WriteLine($"My name is {this.name} is a {this.age} years old.");
+    }
+
+    public string Name => name;
+    public int Age => age;
+}
+
+class Employes : Person
+{
+    public Employes(string name, int age) : base(name, age)
+    { }
+
+}
+
+Employes em1 =  new Employes("John", 1);
+Person per = em1;
